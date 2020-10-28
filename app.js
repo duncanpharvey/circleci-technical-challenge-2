cfa02bf56d9db6700767a59c68a0c0f2e9a28187
Duncan Harvey <duncanpharvey@gmail.com>
2020-10-28 18:55:40 -0400

    sets up web site files

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send('Hello World!')
+    res.sendFile(`${__dirname}/public/index.html`);
 });
+
+app.use("/public", express.static(`${__dirname}/public`));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening on port ${port}`)); // use port if provided, otherwise use 5000
